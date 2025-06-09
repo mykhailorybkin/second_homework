e0f4a7f2524bec1bc69d4b23d6bffaa20312c3b6
Mykhailo Rybkin <mykhailorybkin6@gmail.com>
2025-06-09 21:36:30 +0200

    First version of first task

---
+++ first_task.py
@@ -0,0 +1,23 @@
+def total_salary(path):
+    with open(path) as salary_file:
+        try:
+            sum_sallary = 0
+            
+            number_of_workers = 0
+            for line in salary_file.readlines():
+                if not line.strip():
+                    continue
+                one_salary = line.split(',')
+                sum_sallary = sum_sallary + int(one_salary[1])
+                number_of_workers = number_of_workers + 1
+
+            average_sallary = sum_sallary // number_of_workers
+
+            return(average_sallary, sum_sallary)
+            
+
+        except:
+            return "Error while trying to run the script"
+        
+a = total_salary("salaries.txt")
+print(a)
